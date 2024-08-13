a4b1e3f9989882b1f2237917705840143381e28b
Juho Lee <eungseoni@gmail.com>
2024-08-13 12:20:40 -0400

    remove react-ga

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ReactGA from "react-ga";
-const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID; // 발급받은 추적ID를 환경 변수로 불러온다.
-ReactGA.initialize(TRACKING_ID);
 ReactDOM.render(
   <React.StrictMode>
     <App />
